ac72eed34bd3c68a86e671e6ab85e69acff4c89b
yemiOdetola <yemiotola@outlook.com>
2019-02-05 13:46:59 +0100

    Products API, products with faker

--- routes/seller.js
+++ routes/seller.js
@@ -3,6 +3,7 @@ const Product = require('../models/product');
 const config = require('../config');
 const devConfig = config.development;
 const checkJWT = require('../middlewares/check-jwt');
+const faker = require('faker');
 
 const aws = require('aws-sdk');
 const multer = require('multer');
@@ -44,7 +45,42 @@ router.route('/products')
         });
 
     })
-    .get()
+    .get(checkJWT, (req, res, next) => {
+        Product.find({ owner: req.decoded.user._id})
+            .populate('owner')
+            .populate('category')
+            .exec((err, products) => {
+                if(products) {
+                    res.json({
+                        success: true,
+                        message: 'Users products loaded',
+                        products: products
+                    });
+                } else {
+                    res.json({
+                        success: false,
+                        message: 'Err returning your products....please retry'
+                    })
+                }
+            })
+    });
+
+    // Faker for data pop
+    router.get('/faker/test', (req, res, next) => {
+        for (let i = 0; i < 30; i++) {
+            let product = new Product();
+            product.owner = '5c4aef8431d0682b4727eb29';
+            product.category = '5c4e36cb68787d1fe3ed5e68';
+            product.image = faker.image.nature;
+            product.title = faker.commerce.productName();
+            product.description = faker.lorem.sentences();
+            product.price = faker.commerce.price();
+            product.save()
+        }
+        res.json({
+            message: 'Successfully added dummy products'
+        })
+    })
 
 
 module.exports = router;
